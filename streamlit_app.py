da4c22466c5aa3a5215dc6f07aea7a7e95c6eb2e
otaruit <kimuranatsume5656@gmail.com>
2024-08-06 09:37:58 +0900

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import pandas as pd
+
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
+    st.markdown('### アクセスログ（先頭5件）')
+    st.write(df.head(5))
